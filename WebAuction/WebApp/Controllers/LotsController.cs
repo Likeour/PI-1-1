0f72e3cce423f46822badebb4bb1f53cfafdf177
makskarv <makskarv@mail.ru>
2018-06-04 12:11:33 +0300

    f

--- WebAuction/WebApp/Controllers/LotsController.cs
+++ WebAuction/WebApp/Controllers/LotsController.cs
@@ -3,9 +3,11 @@ using BLL.DTO;
 using BLL.Interfaces;
 using BLL.Services;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using WebApp.Models;
 using WebApp.Ninject;
@@ -33,13 +35,35 @@ namespace WebApp.Controllers
         {
             this.LotsService = LotsService;
         }
-        
-        public ActionResult Index()
-        {
 
-         //   IEnumerable<CategorieDTO> cat = LotsControllerMapper.Map<CategorieDTO, CategoryViewModel>(LotsService.GetAllCategories());
-            return View();
-            
+        public IEnumerable<CategoryViewModel> Get()
+        {
+            return LotsControllerMapper.Map<IEnumerable<CategorieDTO>, IEnumerable<CategoryViewModel>>(LotsService.GetAllCategories());
+        }
+
+        // GET api/<controller>/5
+        public LotViewModel Get(int Id)
+        {
+            return LotsControllerMapper.Map<LotPostDTO, LotViewModel>(LotsService.GetLot(Id));
+        }
+
+        // POST api/<controller>
+
+        public void Post([FromBody]CategoryViewModel Category)
+        {
+            LotsService.AddCategory(LotsControllerMapper.Map<CategoryViewModel, CategorieDTO>(Category));
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int CategoryId, [FromBody]LotViewModel Lot)
+        {
+            LotsService.AddLot(CategoryId, LotsControllerMapper.Map<LotViewModel, LotPostDTO>(Lot));
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int Id)
+        {
+            LotsService.DeleteLot(Id);
         }
 
     }
