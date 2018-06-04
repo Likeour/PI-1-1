b75ea03a4f60a4f632af29ca6035cd5f33e14873
makskarv <makskarv@mail.ru>
2018-06-04 09:36:33 +0300

    reload pr

---
+++ WebAuction/WebAuctionTest/WebAuctionTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BLL.Services;
+using Moq;
+
+using NUnit.Framework;
+using Ninject;
+using Ninject.Modules;
+
+
+namespace WebAuctionTest
+{
+    [TestFixture]
+    public class WebAuctionTest
+    {
+        [Test]
+        public void GetLots()
+        {
+            LotsService lotsService = new LotsService();
+
+          
+            
+        }
+    }
+}
